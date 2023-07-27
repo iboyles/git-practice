268b590b12c435399de4fad6778e6754883586c9
Ian Boyles <ianboyles001@gmail.com>
2023-07-27 16:51:08 +0100

    new nnew

---
+++ Ex7_TDD/src/test/java/com/qa/AccountTest.java
@@ -0,0 +1,26 @@
+package com.qa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+    private Account act;
+    @BeforeEach
+    void setUp() {
+        act = new Account(new String[]{"Link","Visa"},"10-10-22","11223344","1234");
+    }
+    @Test
+    @DisplayName("Match accounts")
+    void compareTo() {
+        Account inputObject = new Account(new String[]{"Link"},"10-10-22","11223344","1234");
+        assertEquals(0, act.compareTo(inputObject), "Login Failed");
+    }
+    @DisplayName("Check account number")
+    void getAccountNumber(){
+        assertEquals("11223344", act.getAccountNumber(), "Account number mismatch");
+    }
+
+}
